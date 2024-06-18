1a1bdb0c2c2b136f90768f2aadd3d9e02055b267
Charles L <charles.lobo@gmail.com>
2024-06-19 09:34:18 +1000

    feat: improve test cases

--- src/__tests__/index.ts
+++ src/__tests__/index.ts
@@ -1,5 +1,28 @@
 import dots, { DotData, memoryPersister, diskPersister } from "../index";
-import { unlink, stat, readFile } from "node:fs/promises";
+import { unlink, readdir, stat, readFile } from "node:fs/promises";
+import { join as pathJoin } from "node:path";
+
+const ROOT_TEST_FOLDER = "/tmp/dots";
+function testdb(n?: number) {
+	if (!n) n = Math.floor(Math.random() * 100) + 1;
+	return `${ROOT_TEST_FOLDER}/${n}`;
+}
+
+async function cleanup() {
+	try {
+		const dbs = await readdir(ROOT_TEST_FOLDER);
+		for (let db of dbs) {
+			db = pathJoin(ROOT_TEST_FOLDER, db);
+			const s = await stat(db);
+			if (s.isFile()) {
+				console.log(`removing ${db}`);
+				await unlink(db);
+			}
+		}
+	} catch (_) {
+		/* ignore */
+	}
+}
 
 type TestRec = {
 	id: number;
@@ -11,43 +34,39 @@ const memCfg = {
 };
 
 describe("setup", () => {
-	const dbname1 = "/tmp/dots/1.db";
 	it("throws exception if shut down", async () => {
-		const dot = await dots.setup<TestRec>(dbname1, memCfg);
+		const dot = await dots.setup<TestRec>(testdb(), memCfg);
 		dot.close();
 		expect(() => dot.add({ id: 1 })).toThrow(`${dot.name} not set up`);
 	});
 
 	it("is ready when starting with empty data", async () => {
 		let ready = false;
-		const dot = await dots.setup<TestRec>(dbname1, memCfg);
+		const dot = await dots.setup<TestRec>(testdb(), memCfg);
 		dot.q((_) => (ready = true));
 		expect(ready).toBe(true);
 	});
 
-	const dbname2 = "/tmp/dots/2.db";
 	it("throws exception if setup twice", async () => {
-		const dot = await dots.setup(dbname2, memCfg);
+		const dot = await dots.setup(testdb(), memCfg);
 		await expect(async () => {
-			await dots.setup(dbname2, memCfg);
+			await dots.setup(dot.name, memCfg);
 		}).rejects.toThrow(`${dot.name} already set up`);
 	});
 
-	const dbname3 = "/tmp/dots/3.db";
 	it("is ready if closed in between", async () => {
 		let ready = 0;
-		let dot = await dots.setup(dbname3, memCfg);
+		let dot = await dots.setup(testdb(), memCfg);
 		dot.q((_) => ready++);
 		await dot.close();
-		dot = await dots.setup(dbname3, memCfg);
+		dot = await dots.setup(dot.name, memCfg);
 		dot.q((_) => ready++);
 		expect(ready).toBe(2);
 	});
 
-	const dbname4 = "/tmp/dots/4.db";
 	it("to be ready and called with data when correctly set up", async () => {
 		const ids: Array<number> = [];
-		const dot = await dots.setup<TestRec>(dbname4, memCfg);
+		const dot = await dots.setup<TestRec>(testdb(), memCfg);
 		dot.add({ id: 1 });
 		dot.q((recs) => recs.forEach((r) => ids.push(r.id)));
 		expect(ids).toStrictEqual([1]);
@@ -58,10 +77,9 @@ describe("setup", () => {
 });
 
 describe("saving", () => {
-	const dbname = "/tmp/dots/1.db";
 	it("saves after 1 second", async () => {
 		let numsaved = 0;
-		const dot = await dots.setup(dbname, {
+		const dot = await dots.setup(testdb(), {
 			saveEvery: 1,
 			persister: memoryPersister<TestRec>(),
 			optimizer: (dotdata: DotData<TestRec>) => (numsaved = dotdata.saved),
@@ -76,7 +94,7 @@ describe("saving", () => {
 	it("does not persist after closing when saving in memory", async () => {
 		let numrecs = 0;
 		let ids: Array<number> = [];
-		let dot = await dots.setup(dbname, {
+		let dot = await dots.setup(testdb(), {
 			saveEvery: 1,
 			persister: memoryPersister<TestRec>(),
 		});
@@ -85,7 +103,7 @@ describe("saving", () => {
 		dot.q((recs) => recs.forEach((r) => ids.push(r.id)));
 		expect(ids).toStrictEqual([1, 2]);
 		await dot.close();
-		dot = await dots.setup(dbname, {
+		dot = await dots.setup(dot.name, {
 			saveEvery: 1,
 			persister: memoryPersister<TestRec>(),
 		});
@@ -98,10 +116,10 @@ describe("saving", () => {
 	});
 
 	it("persists after closing when saving to disk", async () => {
-		await cleanup(dbname);
+		await cleanup();
 		let numrecs = 0;
 		let ids: Array<number> = [];
-		let dot = await dots.setup<TestRec>(dbname, {
+		let dot = await dots.setup<TestRec>(testdb(), {
 			saveEvery: 1,
 			persister: diskPersister<TestRec>(),
 		});
@@ -110,7 +128,7 @@ describe("saving", () => {
 		dot.q((recs) => recs.forEach((r) => ids.push(r.id)));
 		expect(ids).toStrictEqual([1, 2]);
 		await dot.close();
-		dot = await dots.setup<TestRec>(dbname, {
+		dot = await dots.setup<TestRec>(dot.name, {
 			saveEvery: 1,
 			persister: diskPersister<TestRec>(),
 		});
@@ -120,14 +138,14 @@ describe("saving", () => {
 		ids = [];
 		dot.q((recs) => recs.forEach((r) => ids.push(r.id)));
 		expect(ids).toStrictEqual([1, 2, 11]);
-		await cleanup(dbname);
+		await cleanup();
 	});
 
 	it("persists raw", async () => {
-		await cleanup(dbname);
+		await cleanup();
 		let numrecs = 0;
 		let ids: Array<string> = [];
-		let dot = await dots.setup<string>(dbname, {
+		let dot = await dots.setup<string>(testdb(), {
 			saveEvery: 1,
 			persister: diskPersister({ raw: true }),
 		});
@@ -136,7 +154,7 @@ describe("saving", () => {
 		dot.q((recs) => recs.forEach((r) => ids.push(r)));
 		expect(ids).toStrictEqual(["one", "two"]);
 		await dot.close();
-		dot = await dots.setup<string>(dbname, {
+		dot = await dots.setup<string>(dot.name, {
 			saveEvery: 1,
 			persister: diskPersister({ raw: true }),
 		});
@@ -147,20 +165,12 @@ describe("saving", () => {
 		dot.q((recs) => recs.forEach((r) => ids.push(r)));
 		expect(ids).toStrictEqual(["one", "two", "three"]);
 		await dot.close();
-		const data = await readFile(dbname, "utf8");
+		const data = await readFile(dot.name, "utf8");
 		expect(data).toStrictEqual("one\ntwo\nthree\n");
-		await cleanup(dbname);
+		await cleanup();
 	});
 });
+
 afterEach(() => {
 	dots.shutdown();
 });
-
-async function cleanup(dbname: string) {
-	try {
-		const s = await stat(dbname);
-		if (s.isFile()) unlink(dbname);
-	} catch (_) {
-		/* ignore */
-	}
-}
